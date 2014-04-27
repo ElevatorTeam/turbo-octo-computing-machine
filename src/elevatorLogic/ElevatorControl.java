54ac43218d3a44e1d0a6915ec62bdf3bc9580fa7
StarFruitMystic <imgonthier@gmail.com>
2014-04-26 19:49:20 -0500

    added a line with an arraylist

--- src/elevatorLogic/ElevatorControl.java
+++ src/elevatorLogic/ElevatorControl.java
@@ -21,9 +21,11 @@ public class ElevatorControl {
 	//The "nextPassengerLocation" is the amount of people standing outside of the elevators on each floor.
 	//for example, if nextPassengerLocation.get(0) is 3, then there are 3 people standing outside of the elevator on floor 0.
 	//On the other hand, dropPassengerLocation is the floor people inside the elevator want to be dropped off at.
+	//passengerFloor is the number of passengers on  	
 	ArrayList<Integer> nextPassengerLocation = new ArrayList<Integer>();
 	ArrayList<Integer> dropPassengerLocation = new ArrayList<Integer>();
 	ArrayList<Boolean> panelList = new ArrayList<Boolean>();
+	ArrayList<ArrayList<Integer>> passengerFloor = new ArrayList<ArrayList<Integer>>();	
 	String passengersOn = "";
 	String passengersOff = "";
 	
